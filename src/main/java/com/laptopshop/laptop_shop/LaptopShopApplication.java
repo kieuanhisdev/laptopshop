3a7b3e7c2aacd9dc485eb1a74d205bb9ea120d60
KieuAnhDev <Kieuanh.dev@gmail.com>
2025-02-25 17:36:29 +0700

    dashboard admin

--- src/main/java/com/laptopshop/laptop_shop/LaptopShopApplication.java
+++ src/main/java/com/laptopshop/laptop_shop/LaptopShopApplication.java
@@ -1,9 +1,13 @@
 package com.laptopshop.laptop_shop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
+
 public class LaptopShopApplication {
 
 	public static void main(String[] args) {
